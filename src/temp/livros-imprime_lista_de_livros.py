0c8ee32bd0d797aaeabf20feec2db973249d55b0
a85275 <a85275@ualg.pt>
2024-11-28 11:08:26 +0000

    criação da função imprimir lista de livros

--- src/temp/livros-imprime_lista_de_livros.py
+++ src/temp/livros-imprime_lista_de_livros.py
@@ -1,4 +1,13 @@
 def imprime_lista_de_livros(lista_de_livros):
-    """TODO: documentação"""
+    """Esta função vai imprimir uma lista de livros
 
-    imprime_lista(cabecalho="Lista de Livros", lista=lista_de_livros)
+    Args:
+        param1(list): Lista de Livros
+
+    Returns:
+         List: Retorna uma lista de livros
+    """
+
+ imprime_lista(cabecalho="Lista de Livros", lista=lista_de_livros)
+
+
