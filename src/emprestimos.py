3dc05661223cd36c4cb6293595e6730fb6f136f7
a74523 <a74523@ualg.pt>
2024-11-28 11:08:06 +0000

    criaçao da função novo_emprestimo

--- src/emprestimos.py
+++ src/emprestimos.py
@@ -12,3 +12,32 @@ nome_ficheiro_lista_de_emprestimos = "lista_de_emprestimos.pk"
 # todos os outros ficheiros temp/emprestimos-*.py, e inclusive estes comentários
 
 # ...
+
+def cria_novo_emprestimo(lista_de_leitores, lista_de_livros, lista_de_funcionarios):
+    """ Pede ao utilizador para introduzir os dados de um novo emprestimo
+
+        Args:
+            param1(list): lista de leitores
+            param2(list): lista de livros
+            param3(list): lista de funcionarios
+
+
+        returns:
+            dict: dicionario com uma fatura, na forma
+            {"leitor": <<id_leitor>>, "livro": <<id_livro>>, "funcionario": <<id_funcionario>>, "data": <<data>>, ...}
+    """
+
+    id_leitor = pergunta_id(questao="Qual o id do leitor?", lista=lista_de_leitores, mostra_lista=True)
+    id_livro = pergunta_id(questao="Qual o id do livro?", lista=lista_de_livros, mostra_lista=True)
+    id_funcionario = pergunta_id(questao="Qual o id do funcionario?", lista=lista_de_funcionarios, mostra_lista=True)
+    # TODO: Pedir o resto dos dados da fatura, e não esquecer de os guardar no dicionario
+    # ...
+
+    emprestimo = {
+        "leitor": id_leitor,
+        "livro": id_livro,
+        "funcionario": id_funcionario,
+        "data": date.today(),
+    }
+
+    return emprestimo
