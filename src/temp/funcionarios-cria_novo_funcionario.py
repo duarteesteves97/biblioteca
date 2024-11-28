bd807f7214ed5d2c17e8c9ff4c387a4f2b4ba3cf
a74523 <a74523@ualg.pt>
2024-11-28 10:59:15 +0000

    add src ao gitignore e apaguei os ficheiros temp do git

--- src/temp/funcionarios-cria_novo_funcionario.py
+++
@@ -1,9 +0,0 @@
-def cria_novo_funcionario():
-    """Pedir os dados de um novo funcionario
-
-    :return: dicionario com o novo funcionario, {"nome": <<nome>>, "nif": <<nif>>, ...}
-    """
-    # TODO: pedir os dados do funcionario e não esquecer de os devolver
-    # ...
-
-    pass
