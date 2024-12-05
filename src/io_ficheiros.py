60370ca7eca333796ba16a51f4ab7f48ab624834
a74523 <a74523@ualg.pt>
2024-12-05 10:16:45 +0000

    funções do io_terminal para io_ficheiros documentaçao e funçoes do io_ficheiros

--- src/io_ficheiros.py
+++ src/io_ficheiros.py
@@ -12,3 +12,59 @@ from livros import nome_ficheiro_lista_de_livros
 # todos os outros ficheiros temp/io_ficheiros-*.py, e inclusive estes comentários
 
 # ...
+def carrega_as_listas_dos_ficheiros():
+    """
+    Esta função carrega as listas dos ficheiros
+
+    Return: lista de livros, lista de leitores, lista de emprestimos e lista de funcionrios
+    """
+
+    lista_de_livros = le_de_ficheiro(nome_ficheiro_lista_de_livros)
+    lista_de_leitores = le_de_ficheiro(nome_ficheiro_lista_de_leitores)
+    lista_de_emprestimos = le_de_ficheiro(nome_ficheiro_lista_de_emprestimos)
+    lista_de_funcionarios = le_de_ficheiro(nome_ficheiro_lista_de_funcionarios)
+
+    return  lista_de_livros, lista_de_leitores, lista_de_emprestimos, lista_de_funcionarios
+
+def guarda_as_listas_em_ficheiros(lista_de_livros, lista_de_leitores, lista_de_emprestimos, lista_de_funcionarios):
+    """
+    guarda as listas em ficheiros
+
+    :param lista_de_livros:
+    :param lista_de_leitores:
+    :param lista_de_emprestimos:
+    :param lista_de_funcionarios:
+    """
+
+    op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
+    if op in ['s', 'S']:
+        guarda_em_ficheiro(nome_ficheiro_lista_de_livros, lista_de_livros)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_leitores, lista_de_leitores)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_emprestimos, lista_de_emprestimos)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_funcionarios, lista_de_funcionarios)
+    else:
+        print("Gravação cancelada...")
+
+def guarda_em_ficheiro(nome_do_ficheiro, dados):
+    """Guarda os dados recebidos num ficheiro
+
+    :param nome_do_ficheiro: nome do ficheiro onde vai guardar os dados
+    :param dados: dados a serem guardados
+    """
+
+    with open(nome_do_ficheiro, "wb") as f:
+        pickle.dump(dados, f)
+
+def le_de_ficheiro(nome_ficheiro):
+    """Lê os dados de um ficheiro
+
+    :param nome_ficheiro: nome do ficheiro onde estao os dados
+    :return: o que leu do ficheiro (depende dos dados guardados)
+    """
+
+    try:
+        with open(nome_ficheiro, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        print(f"O ficheiro {nome_ficheiro} não existe.")
+        return []
